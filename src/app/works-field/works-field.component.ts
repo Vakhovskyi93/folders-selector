c19cc1ba4996a7d7e6c90976d0788fe6ed4f0466
Oleksandr Vakhovskyi <o.vakhovskyi@gmail.com>
2021-12-15 17:15:42 +0200

    create project & main component

---
+++ src/app/works-field/works-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-works-field',
+  templateUrl: './works-field.component.html',
+  styleUrls: ['./works-field.component.scss']
+})
+export class WorksFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
