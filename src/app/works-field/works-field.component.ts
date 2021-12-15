f210800bf8a46fdc48f20ca850581a9f02420b0f
Oleksandr Vakhovskyi <o.vakhovskyi@gmail.com>
2021-12-15 17:41:41 +0200

    loyout, styles for folders view

--- src/app/works-field/works-field.component.ts
+++ src/app/works-field/works-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-works-field',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksFieldComponent implements OnInit {
 
-  constructor() { }
+  foldersList = [
+    { name: 'First'},
+    { name: 'Second'},
+    { name: 'Third'},
+    { name: 'Fourth'},
+  ]
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
