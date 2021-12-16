fead5e09e259faf9e240615385af53523a40457e
Oleksandr Vakhovskyi <o.vakhovskyi@gmail.com>
2021-12-16 14:19:50 +0200

    get mouse positions after mousedown, moved and mouseup

--- src/app/works-field/works-field.component.ts
+++ src/app/works-field/works-field.component.ts
@@ -1,11 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, AfterViewInit, ViewChild, QueryList, ViewChildren} from '@angular/core';
+import { fromEvent, Subject} from "rxjs";
+import { last, map, share, startWith, switchMap, tap} from "rxjs/operators";
+
+
+export type Rect = Readonly<{ x: number; y: number; width: number; height: number }>;
+
 
 @Component({
   selector: 'app-works-field',
   templateUrl: './works-field.component.html',
   styleUrls: ['./works-field.component.scss']
 })
-export class WorksFieldComponent implements OnInit {
+export class WorksFieldComponent implements OnInit, AfterViewInit {
 
   foldersList = [
     {name: 'First'},
@@ -15,10 +21,44 @@ export class WorksFieldComponent implements OnInit {
   ];
   selectedFoldersList: String[] = []
 
+  @ViewChild('selectArea') selectArea!: ElementRef;
+  @ViewChildren("folder")
+  folder!: QueryList<ElementRef<HTMLElement>>;
+
+  private readonly areas$$ = new Subject<HTMLElement>();
+
+
   constructor() {
   }
 
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    fromEvent<MouseEvent>(this.selectArea.nativeElement, 'mousedown').pipe(
+      switchMap( mousedown =>fromEvent<MouseEvent>(this.selectArea.nativeElement, 'mousemove').pipe(
+          startWith(mousedown),
+          tap((i)=>console.log(i, mousedown, 'mousedown')),
+          switchMap(()=>  fromEvent<MouseEvent>(this.selectArea.nativeElement, 'mouseup')),
+          map(move => ({
+              x: mousedown.offsetX,
+              y: mousedown.offsetY,
+              width: move.offsetX - mousedown.offsetX,
+              height: move.offsetY - mousedown.offsetY
+            }
+          )),
+          tap((i)=>console.log(i)),
+
+      )
+
+
+
+      ),
+
+    ).subscribe(i => console.log(i, 'i'))
+
+
   }
+
 
 }
