a5d380633c90d91e7072e29ae57562a587165156
Oleksandr Vakhovskyi <o.vakhovskyi@gmail.com>
2021-12-20 11:14:14 +0200

    refactoring, Fixed bug,

--- src/app/works-field/works-field.component.ts
+++ src/app/works-field/works-field.component.ts
@@ -6,14 +6,15 @@ import {
   QueryList,
   ViewChildren,
 } from '@angular/core';
-import {  fromEvent, of, Subscriber, Subscription } from 'rxjs';
+import {  fromEvent, of, Subscription } from 'rxjs';
 
 import {
   auditTime,
   map,
   startWith,
   switchMap,
-  takeUntil
+  takeUntil,
+  tap
 } from 'rxjs/operators';
 
 @Component({
@@ -30,11 +31,10 @@ export class WorksFieldComponent implements AfterViewInit {
   ];
   selectedFolders: string = '';
   selectedArea = {
-    startPointX: 0,
-    startPointY: 0,
-    width: 0,
-    height: 0,
-  };
+    startPointX:0, 
+    startPointY:0, 
+    width:0, 
+    height:0 };
 
   @ViewChild('workField') workField!: ElementRef;
   @ViewChild('selectedZone') selectedZone!: ElementRef;
@@ -57,37 +57,34 @@ export class WorksFieldComponent implements AfterViewInit {
                     item.nativeElement.offsetLeft + item.nativeElement.offsetWidth > area.nativeElement.offsetLeft &&
                     item.nativeElement.offsetLeft < area.nativeElement.offsetLeft + area.nativeElement.offsetWidth &&
                     item.nativeElement.offsetTop + item.nativeElement.offsetHeight > area.nativeElement.offsetTop &&
-                    item.nativeElement.offsetTop < area.nativeElement.offsetTop + area.nativeElement.offsetHeight) ||
-                    item.nativeElement.offsetLeft < this.selectedArea.startPointX &&
-                    item.nativeElement.offsetHeight > this.selectedArea.startPointY &&
-                    item.nativeElement.offsetLeft + item.nativeElement.offsetWidth > this.selectedArea.startPointX
+                    item.nativeElement.offsetTop < area.nativeElement.offsetTop + area.nativeElement.offsetHeight)
                 )
             ),
-            
-            map((i) => i.map((item) => item.nativeElement.innerText)),
-            map((item) => item.join(', '))
+            map(i => i.map(item => item.nativeElement.innerText).join(', '))
           )
         )
       )
-      .subscribe((data) => {this.selectedFolders = data});
+      .subscribe(data => this.selectedFolders = data);
 
     this.area = fromEvent<MouseEvent>(this.workField.nativeElement, 'mousedown')
       .pipe(
+        tap((mousedown) => { this.selectedArea.startPointX = mousedown.offsetX; this.selectedArea.startPointY = mousedown.offsetY}),
         switchMap((mousedown) =>
           fromEvent<MouseEvent>(this.workField.nativeElement, 'mousemove').pipe(
-            auditTime(50),
             startWith(mousedown),
-            takeUntil(fromEvent<MouseEvent>(window, 'mouseup')),
-            map((move) => ({
-              startPointX: mousedown.offsetX,
-              startPointY: mousedown.offsetY,
-              width: move.offsetX - mousedown.offsetX,
-              height: move.offsetY - mousedown.offsetY,
-            }))
+            takeUntil(fromEvent<MouseEvent>(this.workField.nativeElement, 'mouseup')),
+            map((move) => (
+              {
+                startPointX: mousedown.offsetX,
+                startPointY: mousedown.offsetY,
+                width: move.x - mousedown.x ,
+                height: move.y - mousedown.y
+              }
+            ))
           )
         )
       )
-      .subscribe((data) => {this.selectedArea = data;});
+      .subscribe(data => this.selectedArea = data);
   }
 
   ngOnDestroy(){
@@ -101,47 +98,43 @@ export class WorksFieldComponent implements AfterViewInit {
     const height = this.workField?.nativeElement.offsetHeight;
 
     if (this.selectedArea.width > 0 && this.selectedArea.height > 0) {
-      shapesCoordinates = {
+      return shapesCoordinates = {
         width: `${this.selectedArea.width}px`,
         height: `${this.selectedArea.height}px`,
         left: ` ${this.selectedArea.startPointX}px`,
-        top: `${this.selectedArea.startPointY}px`,
+        top: `${this.selectedArea.startPointY}px`
       };
-      return shapesCoordinates;
     }
 
     if (this.selectedArea.width < 0 && this.selectedArea.height > 0) {
-      shapesCoordinates = {
+      return shapesCoordinates = {
         width: `${this.selectedArea.width * -1}px`,
         height: `${this.selectedArea.height}px`,
         right: ` ${width - this.selectedArea.startPointX}px`,
-        top: `${this.selectedArea.startPointY}px`,
+        top: `${this.selectedArea.startPointY}px`
       };
-      return shapesCoordinates;
     }
     
     if (this.selectedArea.width > 0 && this.selectedArea.height < 0) {
-      shapesCoordinates = {
+      return shapesCoordinates = {
         width: `${this.selectedArea.width}px`,
         height: `${this.selectedArea.height * -1}px`,
         left: ` ${this.selectedArea.startPointX}px`,
-        bottom: `${height - this.selectedArea.startPointY}px`,
+        bottom: `${height - this.selectedArea.startPointY}px`
       };
-      return shapesCoordinates;
     }
 
     else  {
-      shapesCoordinates = {
+      return shapesCoordinates = {
         width: `${this.selectedArea.width * -1}px`,
         height: `${this.selectedArea.height * -1}px`,
         right: ` ${width - this.selectedArea.startPointX}px`,
-        bottom: `${height - this.selectedArea.startPointY}px`,
+        bottom: `${height - this.selectedArea.startPointY}px`
       };
-      return shapesCoordinates;
     }
   }
 
-  selectedFolder(name:string){
+  selectedFolderStyle(name:string){
     return this.selectedFolders.indexOf(name) + 1;
   }
 }
